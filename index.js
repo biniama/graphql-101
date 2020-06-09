f1f76d849737c99523d45422b5db7a0085d85acc
Biniam Asnake <biniam.asnake@kaufda.de>
2020-06-09 23:04:05 +0200

    allTrails, findTrailById, trailCount and definition of parent and args

--- index.js
+++ index.js
@@ -12,10 +12,30 @@ const typeDefs = gql`
         elevationGain: Int!
     }
 
+    enum TrailStatus {
+        OPEN
+        CLOSED
+    }
+
+    type Trail {
+        name: String!
+        lift: [String!]!
+        difficulty: String!
+        status: TrailStatus!
+        gromed: Boolean!
+        snowmaking: Boolean!
+        trees: Boolean!
+        night: Boolean!
+        id: ID!
+    }
+
     type Query {
         allLifts: [Lift!]!
         liftCount: Int!
         findLiftById(id: ID!): Lift!
+        allTrails: [Trail!]!
+        findTrailById(id: ID!): Trail!
+        trailCount(status: TrailStatus): Int!
     }
 `
 
@@ -23,7 +43,16 @@ const resolvers = {
     Query: {
         allLifts: () => lifts,
         liftCount: () => lifts.length,
-        findLiftById: (parent, args) => lifts.find(lift => args.id === lift.id)
+        // args is anything that is coming from the user
+        // parent is the entire data object where we are operating such as Lift or Trail
+        // parent aka root or object or _
+        findLiftById: (parent, args) => lifts.find(lift => args.id === lift.id),
+        allTrails: () => trails,
+        findTrailById: (parent, args) => trails.find(trail => args.id === trail.id),
+        trailCount: (parent, args) =>
+            !args.status
+                ? trails.length
+                : trails.filter(trail => args.status === trail.status).length,
     }
 }
 
